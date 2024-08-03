e45e431df2284468df6aaf545de00b40d6e0096e
yoonwooiOS <yoonwoo4429@kakao.com>
2024-08-03 13:09:04 +0900

    🐛 fix : Error Mesage Enum타입으로 변경

--- SeSACRxThreads/SignUp/PasswordViewController.swift
+++ SeSACRxThreads/SignUp/PasswordViewController.swift
@@ -12,14 +12,9 @@ import RxCocoa
 //
 class PasswordViewController: UIViewController {
    
-    private let passwordTextField = {
-        let textField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
-        
-        textField.keyboardType = .decimalPad
-        return textField
-    }()
- 
+    let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    let stateLabel = UILabel()
     let validText = PublishSubject<String>()
     let disposeBag = DisposeBag()
     override func viewDidLoad() {
@@ -32,10 +27,9 @@ class PasswordViewController: UIViewController {
         
     }
     private func bind() {
-        passwordTextField.rx.text.onNext("010")
         let validation = passwordTextField.rx.text
             .orEmpty
-            .map { $0.count >= 10}
+            .map { $0.count >= 8}
         validation
             .bind(to: nextButton.rx.isEnabled)
             .disposed(by: disposeBag)
@@ -43,20 +37,19 @@ class PasswordViewController: UIViewController {
             .bind(with: self) { owner, value in
                 let color: UIColor = value ? .systemBlue : .systemGray
                 owner.nextButton.backgroundColor = color
-                
+                owner.stateLabel.text = value ? ValidText.passwordError.validPassword.rawValue : ValidText.passwordError.invalidPassword.rawValue
+                owner.stateLabel.textColor = value ? .systemGreen : .systemRed
             }
             .disposed(by: disposeBag)
         nextButton.rx.tap
-            .bind(with: self) { owner, value in
-                print("sdsa")
-                owner.navigationController?.pushViewController(PhoneViewController(), animated: true)
+            .bind(with: self) { onwner, _ in
+                onwner.navigationController?.pushViewController(PhoneViewController(), animated: true)
             }
             .disposed(by: disposeBag)
     }
-   
-    
     func configureLayout() {
         view.addSubview(passwordTextField)
+        view.addSubview(stateLabel)
         view.addSubview(nextButton)
          
         passwordTextField.snp.makeConstraints { make in
@@ -64,10 +57,14 @@ class PasswordViewController: UIViewController {
             make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
-        
+        stateLabel.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(12)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+
+        }
         nextButton.snp.makeConstraints { make in
             make.height.equalTo(50)
-            make.top.equalTo(passwordTextField.snp.bottom).offset(30)
+            make.top.equalTo(stateLabel.snp.bottom).offset(12)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
--- SeSACRxThreads/SignUp/SignUpViewController.swift
+++ SeSACRxThreads/SignUp/SignUpViewController.swift
@@ -15,10 +15,7 @@ import RxCocoa
  email 검증을 한다
  검증에 통과되면 다음 버튼을 눌러서 화면 이동을 한다
  */
-enum emailError: String, Error {
-    case invalidEmail = "이메일은 4자 이상 @가 포함되어야 합니다."
-    case validEmail = "사용 가능한 이메일 입니다"
-}
+
 class SignUpViewController: UIViewController {
     
     let emailTextField = SignTextField(placeholderText: "이메일을 입력해주세요")
@@ -57,7 +54,7 @@ class SignUpViewController: UIViewController {
                 let color: UIColor = value ? .systemBlue : .systemGray
                 owner.nextButton.backgroundColor = color
                 owner.validationButton.isHidden = !value
-                owner.stateLabel.text = value ? emailError.validEmail.rawValue : emailError.invalidEmail.rawValue
+                owner.stateLabel.text = value ? ValidText.emailError.validEmail.rawValue : ValidText.emailError.invalidEmail.rawValue
                 owner.stateLabel.textColor = value ? .systemGreen : .systemRed
             }
             .disposed(by: disposeBag)
@@ -82,7 +79,6 @@ class SignUpViewController: UIViewController {
     
     func configureLayout() {
         view.addSubview(emailTextField)
-        view.addSubview(validationButton)
         view.addSubview(stateLabel)
         view.addSubview(nextButton)
         
@@ -90,7 +86,6 @@ class SignUpViewController: UIViewController {
             make.height.equalTo(50)
             make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
-            make.trailing.equalTo(validationButton.snp.leading).offset(-8)
         }
         stateLabel.snp.makeConstraints { make in
             make.top.equalTo(emailTextField.snp.bottom).offset(8)
@@ -107,21 +102,4 @@ class SignUpViewController: UIViewController {
 
 }
  
-import UIKit
 
-extension UIViewController {
-    
-    func showAlert(t: String, msg: String, style: UIAlertController.Style, ok: String, complitionHandler: @escaping (UIAlertAction) -> Void ) {
-        
-        let alert = UIAlertController(title: t, message: msg, preferredStyle: style)
-        
-        let canel = UIAlertAction(title: "취소", style: .cancel)
-        let ok = UIAlertAction(title: ok, style: .destructive, handler: complitionHandler)
-        
-        alert.addAction(ok)
-        alert.addAction(canel)
-        present(alert, animated: true, completion: nil)
-        
-        
-    }
-}
