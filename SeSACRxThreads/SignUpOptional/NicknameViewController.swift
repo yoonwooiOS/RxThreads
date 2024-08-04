fa34e268c6475e4cef2e2c4a5784cf13482e5698
yoonwooiOS <yoonwoo4429@kakao.com>
2024-08-05 00:13:30 +0900

    ✨ feat : NicknameViewController 기능구현

--- SeSACRxThreads/SignUpOptional/NicknameViewController.swift
+++ SeSACRxThreads/SignUpOptional/NicknameViewController.swift
@@ -7,30 +7,47 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class NicknameViewController: UIViewController {
    
     let nicknameTextField = SignTextField(placeholderText: "닉네임을 입력해주세요")
     let nextButton = PointButton(title: "다음")
-    
+    let nicknameStateLabel = UILabel()
+    let disposeBag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = Color.white
-        
         configureLayout()
-       
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
-
+        bind()
     }
-    
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(BirthdayViewController(), animated: true)
+    private func bind() {
+        let validation = nicknameTextField.rx.text
+            .orEmpty
+            .map { $0.count >= 2 && $0.count <= 10}
+        validation
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        validation
+            .bind(with: self) { owner, value in
+                let nicknameStateLabelColor: UIColor = value ? .systemGreen : .systemRed
+                let nextButtonColor: UIColor = !value ? .systemGray : .systemBlue
+                owner.nicknameStateLabel.textColor = nicknameStateLabelColor
+                owner.nicknameStateLabel.text = value ? ValidText.nicknameError.validNickname.rawValue : ValidText.nicknameError.invalidNickname.rawValue
+                owner.nextButton.backgroundColor = nextButtonColor
+            }
+            .disposed(by: disposeBag)
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.pushViewController(BirthdayViewController(), animated: true)
+            }
+            .disposed(by: disposeBag)
     }
-
     
     func configureLayout() {
         view.addSubview(nicknameTextField)
+        view.addSubview(nicknameStateLabel)
         view.addSubview(nextButton)
          
         nicknameTextField.snp.makeConstraints { make in
@@ -38,10 +55,13 @@ class NicknameViewController: UIViewController {
             make.top.equalTo(view.safeAreaLayoutGuide).offset(200)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
-        
+        nicknameStateLabel.snp.makeConstraints { make in
+            make.top.equalTo(nicknameTextField.snp.bottom).offset(12)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
         nextButton.snp.makeConstraints { make in
             make.height.equalTo(50)
-            make.top.equalTo(nicknameTextField.snp.bottom).offset(30)
+            make.top.equalTo(nicknameStateLabel.snp.bottom).offset(12)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
